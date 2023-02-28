3b41cc971f74feb64ee2bb6dc7eb1822422ed594
chels23 <113195011+chels23@users.noreply.github.com>
2023-02-28 20:28:07 +0100

    inicial

---
+++ WPF Prueba/WPF Prueba/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Prueba
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /*
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // Abrimos una ventana nueva modal
+            Window1 window1 = new Window1();
+            window1.Owner = this;
+            window1.Show();
+        }
+        */
+        private void Open_Login(object sender, RoutedEventArgs e)
+        {
+            // Abrimos una ventana nueva modal
+            Window2 window2 = new Window2();
+            window2.Owner = this;
+            window2.Show();
+        }
+    }
+}
